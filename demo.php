71904dbda71e8e9acfca6a4fa0e76e0c998f44ba
Woraluethai <66040233102@udru.ac.th>
2024-11-26 10:27:07 +0700

    update

--- demo.php
+++ demo.php
@@ -7,6 +7,6 @@
 </head>
 <body>
     <h2>ทดสอบส่งค่าเป็น method GET (ผ่าน URL)</h2>
-    <a href="test_get.php?subject=PHP&web=www.google.com"></a>
+    <a href="test_get.php?subject=PHP&web=www.google.com">คลิ๊ก</a>
 </body>
 </html>
