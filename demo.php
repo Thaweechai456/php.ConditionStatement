50854ac039856547cb1d10536be109b1ff8908e4
Woraluethai <66040233102@udru.ac.th>
2024-11-26 11:05:55 +0700

    update

--- demo.php
+++ demo.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ทอสอบส่งค่าโดยใช้ Method GET</title>
+    <title>ทอสอบรับค่าโดยใช้ Method GET</title>
 </head>
 <body>
     <h2>ทดสอบส่งค่าเป็น method GET (ผ่าน URL)</h2>
